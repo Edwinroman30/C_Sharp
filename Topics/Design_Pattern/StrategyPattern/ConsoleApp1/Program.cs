0a08f25ffc9cc7a757e687f7a914cf0c12295821
Edwin Roman <romanseberino@gmail.com>
2021-11-20 08:26:05 -0400

    Inicializacion de proyecto, para trabajar el patron

---
+++ Topics/Design_Pattern/StrategyPattern/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //XML and JSON 
+            //Funcionalidad de exportar que permite elegir distintos formatos: PDF, CSV, XML, JSON…
+            //https://medium.com/all-you-need-is-clean-code/patr%C3%B3n-estrategia-strategy-pattern-654c6e9d2abe
+        }
+    }
+}
